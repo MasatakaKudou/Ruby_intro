88b930a76372a76ad0a67158968a9357ebf8ba99
MasatakaKudou <b1017027@fun.ac.jp>
2020-02-28 21:57:26 +0900

    [add]特定のファイルを読み書きする方法を理解できた:

---
+++ chapter_12/read_and_write_file.rb
@@ -0,0 +1,33 @@
+# CSV
+require 'csv'
+CSV.open('./lib/sample.csv', 'w') do |csv|
+  csv << ['Name','Email', 'Age']
+  csv << ['Alice', 'alice@example.com', 20]
+end
+CSV.foreach('./lib/sample.csv', col_sep: "\t") do |row|
+  puts "1: #{row[0]}, 2: #{row[1]}, 3: #{row[2]}"
+end
+
+puts '-------------------------------------------------------'
+
+# JSON
+require 'json'
+user = { name: 'Alice', email: 'alice@example.com', age: 20 }
+user_json = user.to_json
+puts user_json
+p JSON.parse(user_json)
+
+puts '-------------------------------------------------------'
+
+# YAML
+require 'yaml'
+yaml = <<TEXT
+alice:
+  name: 'Alice'
+  email: 'alice@example.com'
+  age: 20
+TEXT
+users = YAML.load(yaml)
+p users
+users['alice']['gender'] = :female
+puts YAML.dump(users)
